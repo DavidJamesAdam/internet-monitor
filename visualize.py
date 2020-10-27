3401a53142d3e30e457c9545749391bcd1dba52c
andybyers21 <andy.byers21@gmail.com>
2020-10-27 12:13:42 +0000

    write output to csv

--- visualize.py
+++ visualize.py
@@ -3,5 +3,15 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as tick
 
 times = []
-downloaded = []
+download = []
 upload = []
+
+with open('test.csv', 'r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    next(csvfile)
+    for row in plots:
+        times.append(str(row[0]))
+        download.append(float(row[1]))
+        upload.append(float(row[2]))
+
+print(times, "\n", download, "\n", upload)
